4c8d8bc6eaba9dfb2f3d18e7a01b8734beca00bc
AnimusPEXUS <animuspexusagutilities@gmail.com>
2024-02-29 16:45:08 +0300

    worker01 wip

--- worker01/Worker01.cpp
+++ worker01/Worker01.cpp
@@ -2,4 +2,103 @@
 #include <wayround_i2p/ccutils/worker01/Worker01.hpp>
 
 namespace wayround_i2p::ccutils::worker01
-{}
+{
+
+Worker01::Worker01(Worker01ThreadFunction f) :
+    threaded_function(f),
+    _status(stopped)
+{
+}
+
+Worker01::~Worker01()
+{
+}
+
+int Worker01::start()
+{
+    start_stop_mutex.lock();
+    std::experimental::scope_exit(
+        [&]()
+        {
+            start_stop_mutex.unlock();
+        }
+    );
+
+    if (!isStopped())
+    {
+        return 1;
+    }
+
+    _status = starting;
+
+    stop_flag = false;
+    thr       = std::thread(&Worker01::threaded_function_wrapper, this);
+
+    return 0;
+}
+
+void Worker01::stop()
+{
+    start_stop_mutex.lock();
+    std::experimental::scope_exit(
+        [&]()
+        {
+            start_stop_mutex.unlock();
+        }
+    );
+    stop_flag = true;
+    _status   = stopping;
+}
+
+int Worker01::restart()
+{
+    int err = 0;
+
+    stop();
+
+    auto f = futureForStop();
+
+    f.wait();
+
+    err = start();
+    if (err != 0)
+    {
+        return err;
+    }
+
+    return 0;
+}
+
+WorkerStatus Worker01::status()
+{
+    return _status;
+}
+
+bool Worker01::isStopped()
+{
+    return _status == stopped;
+}
+
+std::shared_future<void> Worker01::futureForStop()
+{
+    return stop_promise.get_future().share();
+}
+
+void Worker01::threaded_function_wrapper()
+{
+    std::experimental::scope_exit(
+        [&]()
+        {
+            _status = stopped;
+            stop_promise.set_value();
+        }
+    );
+    _status = working;
+    threaded_function(
+        [&]() -> bool
+        { return stop_flag; }
+    );
+    stop();
+}
+
+} // namespace wayround_i2p::ccutils::worker01
--- worker01/Worker01.hpp
+++ worker01/Worker01.hpp
@@ -1,8 +1,56 @@
 #ifndef WAYROUND_I2P_20240227_051927_170667
 #define WAYROUND_I2P_20240227_051927_170667
 
-namespace wayround_i2p::ccutils::worker01
-{}
+#include <functional>
+#include <future>
+#include <memory>
+#include <mutex>
 
+#include <experimental/scope>
+
+namespace wayround_i2p::ccutils::worker01
+{
+
+enum WorkerStatus : unsigned char
+{
+    stopped,
+    starting,
+    working,
+    stopping
+};
+
+using Worker01ThreadFunction = std::function<void(std::function<bool()>)>;
+
+class Worker01
+{
+  public:
+    // NOTE: this is intentionnaly without ready to use std::shared_ptr creator:
+    //       because, imo, most of the time you wish to use it on stack.
+    //       so create via std::shared_ptr, if you need this, manually.
+
+    // static std::shared_ptr<Worker01> create(Worker01ThreadFunction f);
+
+    Worker01(Worker01ThreadFunction f);
+    ~Worker01();
+
+    int                      start();
+    void                     stop();
+    int                      restart();
+    WorkerStatus             status();
+    bool                     isStopped();
+    std::shared_future<void> futureForStop();
+
+  private:
+    std::thread                  thr;
+    const Worker01ThreadFunction threaded_function;
+    void                         threaded_function_wrapper();
+
+    WorkerStatus       _status   = stopped;
+    bool               stop_flag = false;
+    std::mutex         start_stop_mutex;
+    std::promise<void> stop_promise;
+};
+
+} // namespace wayround_i2p::ccutils::worker01
 
 #endif
